f9c6dfc428772e5f48a87b8cb115d24aefc1feaf
yukidee945 <163768932+yukidee945@users.noreply.github.com>
2026-01-31 02:18:30 +0300

    init

---
+++ IzhSights/reg.js
@@ -0,0 +1,13 @@
+
+function show ( id )
+{
+  var elem = document.getElementById(id); 
+  if ( elem ) 
+    elem.style.display = "block";
+}
+function del ( id )
+{
+  var elem = document.getElementById(id); 
+  if ( elem ) 
+    elem.style.display = "none";
+}
